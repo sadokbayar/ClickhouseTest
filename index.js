0e1c8d332a1915af24870fa773bbc2c3baceaf92
sadokbayar <105717619+sadokbayar@users.noreply.github.com>
2022-05-17 16:34:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,19 +10,8 @@ async function getRandomNumber() {
 
 // Plugin method that runs on plugin load
 async function setupPlugin({ config }) {
-    console.log(config.greeting);
-	console.log('test1---------');
+  console.log('-----------test1---------');
 
-       const { ClickHouse } = require('clickhouse');
-console.log('test2---------');
-const ch = new ClickHouse({ 'click.54.37.31.13.nip.io', '30851', 'clickhouse_operator', 'clickhouse_operator_password' });
-console.log('test3---------');
-const stream = ch.query("SELECT 1", (err, data) => {});
-stream.pipe(process.stdout);
-
-const r = await ch.querying('select* from events limit 2');
-	
- console.log('r='+r);	
 }
 
 // Plugin method that processes event
