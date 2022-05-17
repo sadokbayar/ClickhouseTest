ca8997a02d40ac7f874b743f685e631d90be85bf
sadokbayar <105717619+sadokbayar@users.noreply.github.com>
2022-05-17 16:22:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /* eslint no-undef: "error" */
 
 // Some internal library function
-import { ClickHouse } from 'clickhouse'
+
 
 async function getRandomNumber() {
     return 4
@@ -12,44 +12,16 @@ async function getRandomNumber() {
 async function setupPlugin({ config }) {
     console.log(config.greeting);
 	 console.log('test---------');
+	const ClickHouse = require('@posthog/clickhouse')
+const ch = new ClickHouse({ 'click.54.37.31.13.nip.io', 30851, 'clickhouse_operator', 'clickhouse_operator_password' })
+	 console.log('test1---------');
+const stream = ch.query("SELECT 1", (err, data) => {})
+stream.pipe(process.stdout)
+
+// promise interface, not recommended for selects
+const r = await ch.querying("select* from events limit 2");
 	
-	const clickhouse = new ClickHouse({
-	
-	url: 'https://click.54.37.31.13.nip.io',
-	port: 30851,
-	debug: false,
-	basicAuth: {
-	username: 'clickhouse_operator',
-	password: 'clickhouse_operator_password',
-	},
-	isUseGzip: false,
-	trimQuery: false,
-	usePost: false,
-	format: "json", // "json" || "csv" || "tsv"
-	raw: false,
-	config: {
-		session_id                              : '',
-		session_timeout                         : 60,
-		output_format_json_quote_64bit_integers : 0,
-		enable_http_compression                 : 0,
-		database                                : 'posthog',
-	},
-	
-	// This object merge with request params (see request lib docs)
-	/* reqParams: {
-		...
-	}*/
-});
-	console.log('tttttt');	
-	const queries = ['select* from events limit 2'];
-console.log('queries='+queries);	
-	for(const query of queries) {
-		console.log(query);
-	const r = await clickhouse.query(query).toPromise();
-console.log(r);
-	console.log(query, r);
-}
-	
+ console.log('test1---------');	
 }
 
 // Plugin method that processes event
