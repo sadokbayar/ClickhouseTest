0bafad707245a85c52980b5a3ad2f3ff524a44a1
sadokbayar <105717619+sadokbayar@users.noreply.github.com>
2022-05-17 16:30:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ async function getRandomNumber() {
 async function setupPlugin({ config }) {
     console.log(config.greeting);
 	console.log('test1---------');
-const ClickHouse = require('@posthog/clickhouse');
+
+       const { ClickHouse } = require('@posthog/clickhouse');
 console.log('test2---------');
 const ch = new ClickHouse({ 'click.54.37.31.13.nip.io', 30851, 'clickhouse_operator', 'clickhouse_operator_password' });
 console.log('test3---------');
